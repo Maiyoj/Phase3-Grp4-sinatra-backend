51fe569d950fbed9a42d27252185e81e5a7c8527
Maiyoj <valentinemaiyo1995@gmail.com>
2022-09-03 14:58:02 +0300

    [add] config files settings  and application controller

---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+puts "🌱 Seeding messages..."
+
+Message.create([
+  {
+    resturant: "villarosa"
+    body: "Great services",
+    username: "Liza"
+  },
+  {
+    resturant: "villarosa"
+    body: "Hi!",
+    username: "Duane"
+  },
+  {
+    resturant: "villarosa"
+    body: "let's get this chat app working",
+    username: "Jay"
+  },
+  {
+    resturant: "villarosa"
+    body: "ngl, this looks like a lot 😬",
+    username: "Anna"
+  },
+  {
+    resturant: "villarosa"
+    body: "AWesome services 💪",
+    username: "Harvey"
+  }
+])
+
+puts "✅ Done seeding!"
